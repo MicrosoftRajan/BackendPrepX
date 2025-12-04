84cb6be4ec84de36228840d3240e7027b5961813
MicrosoftRajan <rajanvimla27@gmail.com>
2025-12-04 21:30:23 +0530

    Backend and DataServer Done

--- Backend-3/DataBase.js
+++ Backend-3/DataBase.js
@@ -1,4 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({path:"./.env"})
 
 const pass = "hNdGGzR0FTIYFBmk"
-const db = `mongodb+srv://rajanvimla27_db_user:${pass}@cluster0.qbi2ag4.mongodb.net/?appName=Cluster0`
+const db = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.qbi2ag4.mongodb.net/?appName=Cluster0`
+
+mongoose.connect(db).then(function(conn){
+    console.log("connected to db")
+}).catch(err => console.log(err))
